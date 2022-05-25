54d064a25b5e2cf7d1f7d949fddce9b71af074f1
KevinLin <dolphin0317@gmail.com>
2022-05-25 16:50:42 +0800

    1. 關閉反解   2.加入 TLS

--- shell_vsftpd/run-vsftpd.sh
+++ shell_vsftpd/run-vsftpd.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export VSFTP_PATH='/etc/vsftpd/'
+export VSFTP_CONFIG='vsftpd.conf'
+
 # If no env var for FTP_USER has been specified, use 'admin':
 if [ "$FTP_USER" = "**String**" ]; then
     export FTP_USER='admin'
@@ -21,43 +24,45 @@ fi
 mkdir -p "/home/vsftpd/${FTP_USER}"
 chown -R ftp:ftp /home/vsftpd/
 
-echo -e "${FTP_USER}\n${FTP_PASS}" > /etc/vsftpd/virtual_users.txt
-/usr/bin/db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
+echo -e "${FTP_USER}\n${FTP_PASS}" > ${VSFTP_PATH}virtual_users.txt
+/usr/bin/db_load -T -t hash -f ${VSFTP_PATH}virtual_users.txt ${VSFTP_PATH}virtual_users.db
 
 # Set passive mode parameters:
 if [ "$PASV_ADDRESS" = "**IPv4**" ]; then
     export PASV_ADDRESS=$(/sbin/ip route|awk '/default/ { print $3 }')
 fi
 
-echo "pasv_address=${PASV_ADDRESS}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_max_port=${PASV_MAX_PORT}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_min_port=${PASV_MIN_PORT}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_addr_resolve=${PASV_ADDR_RESOLVE}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_enable=${PASV_ENABLE}" >> /etc/vsftpd/vsftpd.conf
-echo "file_open_mode=${FILE_OPEN_MODE}" >> /etc/vsftpd/vsftpd.conf
-echo "local_umask=${LOCAL_UMASK}" >> /etc/vsftpd/vsftpd.conf
-echo "xferlog_std_format=${XFERLOG_STD_FORMAT}" >> /etc/vsftpd/vsftpd.conf
-echo "reverse_lookup_enable=${REVERSE_LOOKUP_ENABLE}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_promiscuous=${PASV_PROMISCUOUS}" >> /etc/vsftpd/vsftpd.conf
-echo "port_promiscuous=${PORT_PROMISCUOUS}" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_address=${PASV_ADDRESS}" >> ${VSFTP_PATH}${VSFTP_CONFIG}
+echo "pasv_max_port=${PASV_MAX_PORT}" >> ${VSFTP_PATH}${VSFTP_CONFIG}
+echo "pasv_min_port=${PASV_MIN_PORT}" >> ${VSFTP_PATH}${VSFTP_CONFIG}
+echo "pasv_addr_resolve=${PASV_ADDR_RESOLVE}" >> ${VSFTP_PATH}${VSFTP_CONFIG}
+echo "pasv_enable=${PASV_ENABLE}" >> ${VSFTP_PATH}${VSFTP_CONFIG}
+echo "file_open_mode=${FILE_OPEN_MODE}" >> ${VSFTP_PATH}${VSFTP_CONFIG}
+echo "local_umask=${LOCAL_UMASK}" >> ${VSFTP_PATH}${VSFTP_CONFIG}
+echo "xferlog_std_format=${XFERLOG_STD_FORMAT}" >> ${VSFTP_PATH}${VSFTP_CONFIG}
+echo "pasv_promiscuous=${PASV_PROMISCUOUS}" >> ${VSFTP_PATH}${VSFTP_CONFIG}
+echo "port_promiscuous=${PORT_PROMISCUOUS}" >> ${VSFTP_PATH}${VSFTP_CONFIG}
+
+echo "## 關閉反解查詢" >> ${VSFTP_PATH}${VSFTP_CONFIG}
+echo "reverse_lookup_enable=${REVERSE_LOOKUP_ENABLE}" >> ${VSFTP_PATH}${VSFTP_CONFIG}
 
 # Add ssl options
 if [ "$SSL_ENABLE" = "YES" ]; then
-	echo "ssl_enable=YES" >> /etc/vsftpd/vsftpd.conf
-	echo "allow_anon_ssl=NO" >> /etc/vsftpd/vsftpd.conf
-	echo "force_local_data_ssl=YES" >> /etc/vsftpd/vsftpd.conf
-	echo "force_local_logins_ssl=YES" >> /etc/vsftpd/vsftpd.conf
-	echo "ssl_tlsv1=YES" >> /etc/vsftpd/vsftpd.conf
-	echo "ssl_sslv2=NO" >> /etc/vsftpd/vsftpd.conf
-	echo "ssl_sslv3=NO" >> /etc/vsftpd/vsftpd.conf
-	echo "require_ssl_reuse=YES" >> /etc/vsftpd/vsftpd.conf
-	echo "ssl_ciphers=HIGH" >> /etc/vsftpd/vsftpd.conf
-	echo "rsa_cert_file=/etc/vsftpd/cert/$TLS_CERT" >> /etc/vsftpd/vsftpd.conf
-	echo "rsa_private_key_file=/etc/vsftpd/cert/$TLS_KEY" >> /etc/vsftpd/vsftpd.conf
+	echo "ssl_enable=YES" >> ${VSFTP_PATH}${VSFTP_CONFIG}
+	echo "allow_anon_ssl=NO" >> ${VSFTP_PATH}${VSFTP_CONFIG}
+	echo "force_local_data_ssl=YES" >> ${VSFTP_PATH}${VSFTP_CONFIG}
+	echo "force_local_logins_ssl=YES" >> ${VSFTP_PATH}${VSFTP_CONFIG}
+	echo "ssl_tlsv1=YES" >> ${VSFTP_PATH}${VSFTP_CONFIG}
+	echo "ssl_sslv2=NO" >> ${VSFTP_PATH}${VSFTP_CONFIG}
+	echo "ssl_sslv3=NO" >> ${VSFTP_PATH}${VSFTP_CONFIG}
+	echo "require_ssl_reuse=YES" >> ${VSFTP_PATH}${VSFTP_CONFIG}
+	echo "ssl_ciphers=HIGH" >> ${VSFTP_PATH}${VSFTP_CONFIG}
+	echo "rsa_cert_file=${VSFTP_PATH}cert/$TLS_CERT" >> ${VSFTP_PATH}${VSFTP_CONFIG}
+	echo "rsa_private_key_file=${VSFTP_PATH}cert/$TLS_KEY" >> ${VSFTP_PATH}${VSFTP_CONFIG}
 fi
 
 # Get log file path
-export LOG_FILE=`grep xferlog_file /etc/vsftpd/vsftpd.conf|cut -d= -f2`
+export LOG_FILE=`grep xferlog_file ${VSFTP_PATH}${VSFTP_CONFIG}|cut -d= -f2`
 
 # stdout server info:
 if [ ! $LOG_STDOUT ]; then
@@ -81,4 +86,4 @@ else
 fi
 
 # Run vsftpd:
-&>/dev/null /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
+&>/dev/null /usr/sbin/vsftpd ${VSFTP_PATH}${VSFTP_CONFIG}
